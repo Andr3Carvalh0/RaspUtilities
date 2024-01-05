f1796911bb16b512afbd6daf5628f281254baf02
André Carvalho <andre.daniel.carvalho@gmail.com>
2024-01-05 18:11:55 +0100

    Update rasp-banner so we dont have to depend on screenfetch

--- initialize.sh
+++ initialize.sh
@@ -8,7 +8,6 @@
 readonly SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Override login banner
-apt install screenfetch
 touch ~/.hushlogin # Disable default login message
 mkdir -p "/boot/utilities" && cp "${SCRIPT_DIRECTORY}/rasp-banner.sh" "/boot/utilities/"
 echo "if [[ -n \$SSH_CONNECTION ]] ; then
--- rasp-banner.sh
+++ rasp-banner.sh
@@ -9,30 +9,17 @@ readonly DIETPI_GREEN='\e[38;5;154m'
 readonly BOLD_WHITE='\e[1m'
 readonly RESET='\e[0m'
 
-bannerOutput=$(/boot/dietpi/func/dietpi-banner 1)
-screenfetchOutput=$(screenfetch -n -N)
-
-diskValue=$(echo "$screenfetchOutput" | grep "Disk")
-diskValue=$(echo -e "${diskValue/Disk:/${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}Hard drive(s)${RESET} ${DIETPI_GREEN}:${RESET}}")
-
-ramValue=$(echo "$screenfetchOutput" | grep "RAM")
-ramValue=$(echo -e "${ramValue/RAM:/${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}RAM${RESET} ${DIETPI_GREEN}:${RESET}}")
-
-processedValue=$(echo "${bannerOutput/Freespace (RootFS)/SD card}")
+sdCardValue=$(echo -e " ${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}SD Card${RESET} ${DIETPI_GREEN}:${RESET} $(df -h --output=avail,size / | mawk 'NR==2 {printf "%s available / %s total", $1, $2}' 2>&1)")
+diskValue=$(echo -e " ${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}Hard drive(s)${RESET} ${DIETPI_GREEN}:${RESET} $(df -h -x aufs -x tmpfs -x overlay -x drvfs -x devtmpfs  --total | grep "total" | mawk '{printf "%s available / %s total", $4, $2}' 2>&1)")
+ramValue=$(echo -e " ${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}RAM usage${RESET} ${DIETPI_GREEN}:${RESET} $(free -h | mawk 'NR==2 {printf "%s used / %s total", $3, $2}' 2>&1)")
 
 output=""
 
 while IFS= read -r line; do
         transformedLine="$line"
 
-        if [[ "$transformedLine" == *"SD card"* ]]; then
-                totalSize=$(df -h --output=size / | mawk 'NR==2 {print $1}' 2>&1)
-                percentage=$(df -h --output=pcent / | mawk 'NR==2 {print $1}' 2>&1)
-                transformedLine="${transformedLine} / ${totalSize} ($percentage)\n${diskValue}"
-        fi
-
         if [[ "$transformedLine" == *"CPU temp"* ]]; then
-                transformedLine="${transformedLine}\n${ramValue}"
+                transformedLine="${transformedLine}\n${ramValue}\n${sdCardValue}\n${diskValue}"
         fi
 
         if [[ -z $output ]]; then
@@ -40,6 +27,6 @@ while IFS= read -r line; do
         else
                 output="${output}\n${transformedLine}"
         fi
-done <<<"$processedValue"
+done <<<"$(/boot/dietpi/func/dietpi-banner 1)"
 
 echo -e "${output}\n"
