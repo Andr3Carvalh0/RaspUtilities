1721ac006f209268243d9585870f9607bff2f55d
André Carvalho <andre.daniel.carvalho@gmail.com>
2024-01-08 10:01:40 +0100

    Display disk usage instead of free storage

--- rasp-banner.sh
+++ rasp-banner.sh
@@ -9,9 +9,9 @@ readonly DIETPI_GREEN='\e[38;5;154m'
 readonly BOLD_WHITE='\e[1m'
 readonly RESET='\e[0m'
 
-sdCardValue=$(echo -e " ${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}SD Card${RESET} ${DIETPI_GREEN}:${RESET} $(df -h --output=avail,size / | mawk 'NR==2 {printf "%s available / %s total", $1, $2}' 2>&1)")
-diskValue=$(echo -e " ${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}Hard drive(s)${RESET} ${DIETPI_GREEN}:${RESET} $(df -h -x aufs -x tmpfs -x overlay -x drvfs -x devtmpfs  --total | grep "total" | mawk '{printf "%s available / %s total", $4, $2}' 2>&1)")
-ramValue=$(echo -e " ${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}RAM usage${RESET} ${DIETPI_GREEN}:${RESET} $(free -h | mawk 'NR==2 {printf "%s used / %s total", $3, $2}' 2>&1)")
+sdCardValue=$(echo -e " ${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}SD Card usage${RESET} ${DIETPI_GREEN}:${RESET} $(df -h --output=used,size,pcent / | mawk 'NR==2 {printf $1" of "$2" ("$3"%)"}' 2>&1)")
+diskValue=$(echo -e " ${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}Hard drive(s) usage${RESET} ${DIETPI_GREEN}:${RESET} $(df -h -x aufs -x tmpfs -x overlay -x drvfs -x devtmpfs  --total | grep "total" | mawk '{printf $3" of "$2" ("$5"%)"}' 2>&1)")
+ramValue=$(echo -e " ${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}RAM usage${RESET} ${DIETPI_GREEN}:${RESET} $(free -h | mawk 'NR==2 {printf $3" of "$2}' 2>&1)")
 
 output=""
 
