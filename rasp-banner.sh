e16f63fc104505f8db28600aab46a3a5b60aaad2
André Carvalho <andre.daniel.carvalho@gmail.com>
2024-01-08 11:08:02 +0100

    Adds percentage of used ram

--- rasp-banner.sh
+++ rasp-banner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Created by Andre Carvalho on 5th January 2024
-# Last modified: 5th January 2024
+# Last modified: 8th January 2024
 #
 # Custom banner for a DietPi installation
 #
@@ -9,9 +9,10 @@ readonly DIETPI_GREEN='\e[38;5;154m'
 readonly BOLD_WHITE='\e[1m'
 readonly RESET='\e[0m'
 
-sdCardValue=$(echo -e " ${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}SD Card usage${RESET} ${DIETPI_GREEN}:${RESET} $(df -h --output=used,size,pcent / | mawk 'NR==2 {printf $1" of "$2" ("$3"%)"}' 2>&1)")
-diskValue=$(echo -e " ${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}Hard drive(s) usage${RESET} ${DIETPI_GREEN}:${RESET} $(df -h -x aufs -x tmpfs -x overlay -x drvfs -x devtmpfs  --total | grep "total" | mawk '{printf $3" of "$2" ("$5"%)"}' 2>&1)")
-ramValue=$(echo -e " ${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}RAM usage${RESET} ${DIETPI_GREEN}:${RESET} $(free -h | mawk 'NR==2 {printf $3" of "$2}' 2>&1)")
+sdCardValue=$(echo -e " ${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}SD Card usage${RESET} ${DIETPI_GREEN}:${RESET} $(df -h --output=used,size,pcent / | mawk 'NR==2 {printf $1" / "$2" ("$3"%)"}' 2>&1)")
+diskValue=$(echo -e " ${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}Hard drive(s) usage${RESET} ${DIETPI_GREEN}:${RESET} $(df -h -x aufs -x tmpfs -x overlay -x drvfs -x devtmpfs  --total | grep "total" | mawk '{printf $3" / "$2" ("$5"%)"}' 2>&1)")
+ramValue=$(echo -e " ${DIETPI_GREEN}-${RESET} ${BOLD_WHITE}RAM usage${RESET} ${DIETPI_GREEN}:${RESET} $(free -h | mawk 'NR==2 {printf $3" / "$2}' 2>&1)")
+ramPercentage="$(free -b | mawk 'NR==2 {printf "%.0f", 100*$3/$2}' 2>&1)"
 
 output=""
 
@@ -19,14 +20,10 @@ while IFS= read -r line; do
         transformedLine="$line"
 
         if [[ "$transformedLine" == *"CPU temp"* ]]; then
-                transformedLine="${transformedLine}\n${ramValue}\n${sdCardValue}\n${diskValue}"
+                transformedLine="${transformedLine}\n${ramValue} (${ramPercentage}%)\n${sdCardValue}\n${diskValue}"
         fi
 
-        if [[ -z $output ]]; then
-                output="${transformedLine}"
-        else
-                output="${output}\n${transformedLine}"
-        fi
+        output="${output}${transformedLine}\n"
 done <<<"$(/boot/dietpi/func/dietpi-banner 1)"
 
-echo -e "${output}\n"
+echo -e "${output}"
